26296d2fb5c96c01199d89857636c32073d4da38
Kavin291104 <kavin29114@gmail.com>
2026-03-13 10:13:21 +0530

    Update script.js

--- E commerce/frontend/js/script.js
+++ E commerce/frontend/js/script.js
@@ -8,7 +8,7 @@ let allProducts = []
 
 async function loadProducts(){
 
-const res = await fetch("http://localhost:5000/api/products")
+const res = await fetch("https://e-commerce-qrch.onrender.com/")
 
 allProducts = await res.json()
 
@@ -381,7 +381,7 @@ items: orderItems
 
 try{
 
-await fetch("http://localhost:5000/api/orders",{
+await fetch("https://e-commerce-qrch.onrender.com/",{
 
 method:"POST",
 
